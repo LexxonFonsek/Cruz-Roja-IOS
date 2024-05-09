559c86f1df75bce231f1c6860cc9205cbb2347c3
LexxonFonsek <148784448+LexxonFonsek@users.noreply.github.com>
2024-05-09 14:29:49 -0600

    Add files via upload

---
+++ CRUZ ROJA/CRUZ_ROJAApp.swift
@@ -0,0 +1,18 @@
+//
+//  CRUZ_ROJAApp.swift
+//  CRUZ ROJA
+//
+//  Created by Mac OS lab on 09/04/24.
+//
+
+import SwiftUI
+import CoreLocation
+@main
+
+struct CRUZ_ROJAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
